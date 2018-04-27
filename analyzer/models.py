c025c816d5c4ff8d6abb79757adfdaee4cbe1c61
artemka <avanglion59@gmail.com>
2018-04-27 11:29:19 +0300

    Update blockchain support

--- analyzer/models.py
+++ analyzer/models.py
@@ -1,11 +1,13 @@
-import uuid
 import random
+import uuid
 from datetime import datetime, timedelta
 from hashlib import sha256
 
 from django.contrib.auth.models import User
-from django.db import models
 from django.core.serializers import serialize
+from django.db import models
+from django.db.models.signals import pre_save
+from django.dispatch import receiver
 
 
 class SensorType(models.Model):
@@ -37,22 +39,40 @@ class DataItem(models.Model):
     longitude = models.DecimalField(max_digits=8, decimal_places=5)
     previous_hash = models.TextField()
 
-    def save(self, *args, **kwargs):
-        try:
-            self.previous_hash = sha256(serialize('json', [DataItem.objects.last()]).encode('utf-8')).hexdigest()
-        except AttributeError:
-            self.previous_hash = 0
-        super(DataItem, self).save(*args, **kwargs)
-
     @staticmethod
-    def _bootstrap(sensor_id, latitude=47.09514, longitude=37.54131, count=100):
+    def bootstrap_data(sensor_id, latitude=47.09514, longitude=37.54131, count=100):
         timestamp = datetime.now()
         for i in range(count):
             data = round(random.gauss(750, 5))
             timestamp += timedelta(minutes=1)
-            obj = DataItem.objects.create(data=data, timestamp=timestamp, latitude=latitude, longitude=longitude,
-                                          sensor_id=sensor_id)
+            obj = DataItem(data=data, timestamp=timestamp, latitude=latitude, longitude=longitude,
+                           sensor_id=sensor_id)
             obj.save()
+
+    @staticmethod
+    def blockchain_hash(obj):
+        try:
+            return sha256(serialize('json', [obj]).encode('utf-8')).hexdigest()
+        except AttributeError:
+            return 0
+
+    @staticmethod
+    def blockchain_verify():
+        data_items = list(DataItem.objects.order_by('timestamp'))
+        for n, i in enumerate(data_items):
+            try:
+                if DataItem.blockchain_hash(i) == data_items[n + 1].previous_hash:
+                    continue
+                else:
+                    return DataItem.blockchain_hash(i), data_items[n + 1].previous_hash, False
+            except IndexError:
+                continue
+        return True
+
+
+@receiver(pre_save, sender=DataItem)
+def blockchain_hasher(sender, instance, *args, **kwargs):
+    instance.previous_hash = sender.blockchain_hash(sender.objects.order_by('timestamp').last())
 
 
 class Device(models.Model):
