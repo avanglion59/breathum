32bcd234f17c11f99d6cad6f69c200893db63e1b
artemka <avanglion59@gmail.com>
2018-05-03 20:11:17 +0300

    Move to C3

--- assets/js/chart.js
+++ assets/js/chart.js
@@ -18,77 +18,36 @@ function getAsyncManyDataPromises(sensors, from, to) {
 }
 
 function prepareChartData(promises, riskBound, dangerBound) {
-    var datasets = [];
+    var datasets = {
+        xs: {},
+        columns: [],
+        unload: true
+    };
 
-    datasets.push({
-        label: 'Risk Bound',
-        borderColor: 'rgb(192, 0, 0)',
-        backgroundColor: 'rgba(192, 0, 0, .3)',
-        data: [],
-        fill: 'end',
-        pointRadius: 0,
-        pointHoverRadius: 0
-    });
-    datasets.push({
-        label: 'Danger Bound',
-        borderColor: 'rgb(192, 192, 0)',
-        backgroundColor: 'rgba(192, 192, 0, .3)',
-        data: [],
-        fill: 'end',
-        pointRadius: 0,
-        pointHoverRadius: 0
-    });
-
-    // for (var i = 0; i < promises.length; i++) {
-    //     (function (i) {
-    //         promises[i].then(function (result) {
-    //             datasets.push(
-    //                 {
-    //                     label: label,
-    //                     borderColor: 'rgb(0, 192, 0)',
-    //                     backgroundColor: 'rgba(255, 255, 255, 0)',
-    //                     data: [],
-    //                     cubicInterpolationMode: 'monotone'
-    //                 }
-    //             );
-    //
-    //             console.log(datasets[i + 2]);
-    //
-    //             for (var j = 0; j < data.length; j++) {
-    //                 datasets[i + 2].data.push({x: result.labels[j], y: result.data[j]});
-    //                 datasets[0].data.push({x: result.labels[j], y: riskBound});
-    //                 datasets[1].data.push({x: result.labels[j], y: dangerBound});
-    //             }
-    //         }, function (reason) {
-    //             console.log(reason);
-    //         });
-    //     })(i);
-    // }
+    chart.regions([
+        {axis: 'y', start: riskBound, end: dangerBound, class: 'regionRisk'},
+        {axis: 'y', start: dangerBound, class: 'regionDanger'}
+    ]);
 
     Promise.all(promises).then(function (value) {
         for (var i = 0; i < value.length; i++) {
-            datasets.push(
-                {
-                    label: label,
-                    borderColor: 'rgb(' + Math.floor(Math.random() * 256) + ', ' + Math.floor(Math.random() * 256) + ', ' + Math.floor(Math.random() * 256) + ')',
-                    backgroundColor: 'rgba(255, 255, 255, 0)',
-                    data: [],
-                    cubicInterpolationMode: 'monotone'
-                }
-            );
             var result = value[i];
-
+            datasets.columns.push(['sensor' + i]);
             for (var j = 0; j < data.length; j++) {
-                datasets[i + 2].data.push({x: result.labels[j], y: result.data[j]});
-                datasets[0].data.push({x: result.labels[j], y: riskBound});
-                datasets[1].data.push({x: result.labels[j], y: dangerBound});
+                datasets.columns[datasets.columns.length - 1].push(result.data[j]);
             }
+            datasets.columns.push(['sensor' + i + '_x']);
+            for (j = 0; j < data.length; j++) {
+                datasets.columns[datasets.columns.length - 1].push(new Date(result.labels[j]).toISOString());
+            }
+            datasets.xs['sensor' + i] = 'sensor' + i + '_x';
         }
+
+        console.log(datasets);
+        chart.load(datasets);
     }, function (reason) {
         console.log(reason);
     });
-
-    return datasets
 }
 
 function getAsyncEdgesPromise(id) {
@@ -130,75 +89,6 @@ $("#category-select").change(function () {
 
 });
 
-
-ctx = document.getElementById('chart').getContext('2d');
-config = {
-    type: 'line',
-
-    data: {
-        labels: dates,
-        datasets: [{
-            label: 'Danger Bound',
-            borderColor: 'rgb(192, 192, 0)',
-            backgroundColor: 'rgba(192, 192, 0, .3)',
-            data: new Array(data.length).fill(danger),
-            fill: 'end',
-            pointRadius: 0,
-            pointHoverRadius: 0
-        }, {
-            label: 'Risk Bound',
-            borderColor: 'rgb(192, 0, 0)',
-            backgroundColor: 'rgba(192, 0, 0, .3)',
-            data: new Array(data.length).fill(risk),
-            fill: 'end',
-            pointRadius: 0,
-            pointHoverRadius: 0
-        }, {
-            label: label,
-            borderColor: 'rgb(0, 192, 0)',
-            backgroundColor: 'rgba(255, 255, 255, 0)',
-            data: data,
-            cubicInterpolationMode: 'monotone'
-        }]
-    },
-
-    options: {
-        legend: {
-            position: 'bottom'
-        },
-        maintainAspectRatio: false,
-        scales: {
-            xAxes: [{
-                type: 'time',
-                time: {
-                    tooltipFormat: 'll HH:mm'
-                },
-                scaleLabel: {
-                    display: true,
-                    labelString: 'Datetime'
-                }
-            }],
-            yAxes: [{
-                ticks: {
-                    beginAtZero: true
-                },
-                scaleLabel: {
-                    display: true,
-                    labelString: 'Value'
-                }
-            }]
-        },
-        layout: {
-            padding: {
-                left: 25,
-                right: 100,
-                top: 25,
-                bottom: 25
-            }
-        }
-    }
-};
-
 $("#sidebar-btn").sideNav();
 
 $('.datepicker').pickadate({
@@ -214,14 +104,26 @@ $('.datepicker').pickadate({
     }
 });
 
-chart = new Chart(ctx, config);
+var chart = c3.generate({
+    bindto: '#chart',
+    data: {
+        xFormat: '%Y-%m-%dT%H:%M:%S.%LZ',
+        columns: []
+    },
+    axis: {
+        x: {
+            type: 'timeseries',
+            tick: {
+                format: '%Y-%m-%d %H:%M:%S'
+            }
+        }
+    }
+});
 
 setInterval(function () {
     var promises = getAsyncManyDataPromises(sensors, from, to);
     getAsyncEdgesPromise(sensors[0]).then(function (edges) {
-        config.data.datasets = prepareChartData(promises, edges['risk'], edges['danger']);
-        chart.destroy();
-        chart = new Chart(ctx, config);
+        prepareChartData(promises, edges['risk'], edges['danger']);
     }, function (reason) {
         console.log(reason);
     });
@@ -231,17 +133,12 @@ setInterval(function () {
 $("#update-sensors").click(function () {
     sensors = $('#sensor-select').val();
 
-    config.data.labels = [];
-    config.data.datasets = [];
-
     from = $('#from').pickadate('picker').get('select', 'dd/mm/yyyy');
     to = $('#to').pickadate('picker').get('select', 'dd/mm/yyyy');
 
     var promises = getAsyncManyDataPromises(sensors, from, to);
     getAsyncEdgesPromise(sensors[0]).then(function (edges) {
-        config.data.datasets = prepareChartData(promises, edges['risk'], edges['danger']);
-        chart.destroy();
-        chart = new Chart(ctx, config);
+        prepareChartData(promises, edges['risk'], edges['danger']);
     }, function (reason) {
         console.log(reason);
     });
